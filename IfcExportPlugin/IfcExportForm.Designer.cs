ed3d36acfad82a096aeff1086beb44fd554fc0bb
Cory LePage <cory@razorcx.com>
2018-01-14 15:48:45 -0800

    Initial commit

---
+++ IfcExportPlugin/IfcExportForm.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace IfcExportPlugin
+{
+	partial class IfcExportForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IfcExportForm));
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.buttonExport = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::IfcExportPlugin.Properties.Resources.Logo;
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(160, 41);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 7;
+			this.pictureBox1.TabStop = false;
+			// 
+			// buttonExport
+			// 
+			this.buttonExport.Location = new System.Drawing.Point(106, 79);
+			this.buttonExport.Name = "buttonExport";
+			this.buttonExport.Size = new System.Drawing.Size(161, 41);
+			this.buttonExport.TabIndex = 11;
+			this.buttonExport.Text = "Export IFC";
+			this.buttonExport.UseVisualStyleBackColor = true;
+			this.buttonExport.Click += new System.EventHandler(this.buttonExport_Click);
+			// 
+			// IfcExportForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(354, 157);
+			this.Controls.Add(this.buttonExport);
+			this.Controls.Add(this.pictureBox1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "IfcExportForm";
+			this.Text = "IFC Export Plugin Example";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button buttonExport;
+	}
+}
+
---
+++ IfcExportPlugin/IfcExportForm.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Tekla.Structures.Geometry3d;
+using Tekla.Structures.Model;
+using Tekla.Structures.Model.UI;
+
+namespace IfcExportPlugin
+{
+	public partial class IfcExportForm : Form
+	{
+		private readonly Model _model = new Model();
+
+
+		public IfcExportForm()
+		{
+			InitializeComponent();
+		}
+
+		private void buttonExport_Click(object sender, EventArgs e)
+		{
+			var selection = new Picker()
+				.PickObjects(Picker.PickObjectsEnum.PICK_N_OBJECTS, "Pick Objects")
+				.ToAList<ModelObject>();
+
+			var selector = new Tekla.Structures.Model.UI.ModelObjectSelector();
+			selector.Select(new ArrayList(selection), false);
+
+			var modelPath = _model.GetInfo().ModelPath; 
+			var modelName = _model.GetInfo().ModelName.Split('.')[0];
+			ExportIFC($"{modelPath}\\IFC\\OUT_{modelName}");
+		}
+
+		private static void ExportIFC(string outputFileName)
+		{
+			var componentInput = new ComponentInput();
+			componentInput.AddOneInputPosition(new Point(0, 0, 0));
+			var comp = new Component(componentInput)
+			{
+				Name = "ExportIFC",
+				Number = BaseComponent.PLUGIN_OBJECT_NUMBER
+			};
+
+			// Parameters
+			comp.SetAttribute("OutputFile", outputFileName);
+			comp.SetAttribute("Format", 0);
+			comp.SetAttribute("ExportType", 1);
+			//comp.SetAttribute("AdditionalPSets", "");
+			comp.SetAttribute("CreateAll", 0);  // 0 to export only selected objects
+
+			// Advanced
+			comp.SetAttribute("Assemblies", 1);
+			comp.SetAttribute("Bolts", 1);
+			comp.SetAttribute("Welds", 0);
+			comp.SetAttribute("SurfaceTreatments", 1);
+
+			comp.SetAttribute("BaseQuantities", 1);
+			comp.SetAttribute("GridExport", 1);
+			comp.SetAttribute("ReinforcingBars", 1);
+			comp.SetAttribute("PourObjects", 1);
+
+			comp.SetAttribute("LayersNameAsPart", 1);
+			comp.SetAttribute("PLprofileToPlate", 0);
+			comp.SetAttribute("ExcludeSnglPrtAsmb", 0);
+
+			comp.SetAttribute("LocsFromOrganizer", 0);
+
+			comp.Insert();
+		}
+	}
+
+	public static class ExtentionMethods
+	{
+		public static List<T> ToAList<T>(this IEnumerator enumerator)
+		{
+			var list = new List<T>();
+			while (enumerator.MoveNext())
+			{
+				var current = (T)enumerator.Current;
+				if (current != null)
+					list.Add(current);
+			}
+			return list;
+		}
+	}
+}
